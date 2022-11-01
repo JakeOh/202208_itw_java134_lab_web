43df447b417bb5135a7d992dbb1fc8b5a915d141
Jake Oh <jake.oh.lecture@gmail.com>
2022-11-01 12:10:56 +0900

    servlet, request parameter

--- jsp01/src/main/java/edu/web/jsp01/UserInfoServlet.java
+++ jsp01/src/main/java/edu/web/jsp01/UserInfoServlet.java
@@ -22,6 +22,18 @@ public class UserInfoServlet extends HttpServlet {
 	        throws ServletException, IOException {
 		System.out.println("userInfoServlet.doGet() 호출");
 		
+		response.setContentType("text/html; charset=UTF-8");
+		response.getWriter()
+		    .append("<!doctype html>")
+		    .append("<html>")
+		    .append("<body>")
+		    .append("<h1>사용자 정보 전송</h1>")
+		    .append("<form method='post'>")
+		    .append("<input type='text' name='username' />")
+		    .append("<input type='submit' value='제출' />")
+		    .append("</form>")
+		    .append("</body>")
+		    .append("</html>");
 	}
 
 	/**
@@ -31,6 +43,23 @@ public class UserInfoServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 	        throws ServletException, IOException {
 		System.out.println("userInfoServlet.doPost() 호출");
+		
+		// 요청 객체의 인코딩 타입을 설정 <- 요청 파라미터이 한글 처리를 위해서.
+		request.setCharacterEncoding("UTF-8");
+		
+		// 클라이언트가 요청으로 보낸 요청 파라미터(request parameter) 값을 읽음.
+		String username = request.getParameter("username");
+		System.out.println("username = " + username);
+		
+		// 서버에서 클라이언트로 보내는 응답을 작성.
+		response.setContentType("text/html; charset=UTF-8");
+		response.getWriter()
+		    .append("<!doctype html>")
+		    .append("<html>")
+		    .append("<body>")
+		    .append("<h1>안녕하세요, " + username + "!!!</h1>")
+		    .append("</body>")
+		    .append("</html>");
 		
 	}
 
