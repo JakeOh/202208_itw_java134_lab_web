6529cb6d6c0f6b571d78cbbee2bb3f5a752dd1f1
Jake Oh <jake.oh.lecture@gmail.com>
2022-11-02 16:40:02 +0900

    EL, MVC

---
+++ jsp01/src/main/java/edu/web/jsp01/domain/Post.java
@@ -0,0 +1,16 @@
+package edu.web.jsp01.domain;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private Integer id; // 포스트 글 번호
+    private String title; // 포스트 글 제목
+    private String content; // 포스트 글 내용(본문)
+    private LocalDateTime createTime; // 포스트 글 작성 시간
+    
+    // 기본 생성자
+    // 모든 필드를 argument로 갖는 생성자
+    // getters & setters
+    // toString
+
+}
