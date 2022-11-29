209e6015db7eb35e230592a990efb740a6a47c6c
Jake Oh <jake.oh.lecture@gmail.com>
2022-11-29 12:18:13 +0900

    sign-up

--- spring03/src/main/java/com/example/spring03/web/MemberController.java
+++ spring03/src/main/java/com/example/spring03/web/MemberController.java
@@ -3,16 +3,24 @@ package com.example.spring03.web;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.spring03.dto.MemberRegisterDto;
+import com.example.spring03.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/member") // 다른 설정이 없는 경우 GET/POST/PUT/DELETE 모든 방식의 요청을 처리.
 public class MemberController {
 
+    private final MemberService memberService;
+    
     @GetMapping("/signup") // GET 방식의 /member/signup 요청을 처리하는 메서드.
     public void signUp() {
         log.info("signUp() GET");
@@ -27,7 +35,18 @@ public class MemberController {
     public ResponseEntity<String> checkUsername(String username) {
         log.info("checkUsername(username={})", username);
         
-        return null;
+        String result = memberService.checkUsername(username);
+        
+        return ResponseEntity.ok(result);
+    }
+    
+    @PostMapping("/signup")
+    public String signUp(MemberRegisterDto dto) {
+        log.info("signUp(dto={}) POST", dto);
+        
+        memberService.registerMember(dto);
+        
+        return "redirect:/"; // 회원 가입 성공 후 이동(redirect)
     }
     
 }
--- spring03/src/main/resources/static/js/member.js
+++ spring03/src/main/resources/static/js/member.js
@@ -14,13 +14,20 @@ window.addEventListener('DOMContentLoaded', function () {
         const username = usernameInput.value;
         axios
         .get('/member/checkid?username=' + username) // GET Ajax 요청 보냄.
-        .then(response => {
-            console.log(response);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-        
+        .then(response => { displayCheckResult(response.data) }) // 성공(HTTP 200) 응답 콜백
+        .catch(err => { console.log(err); }); // 실패 응답 콜백
     });
+    
+    function displayCheckResult(data) {
+        if (data == 'ok') { // 사용할 수 있는 아이디
+            okDiv.className = 'my-2'; // display: none 속성을 제거 -> 화면에 보임.
+            nokDiv.className = 'my-2 d-none';
+            btnSubmit.classList.remove('disabled'); // 버튼 활성화
+        } else { // 사용할 수 없는 아이디
+            okDiv.className = 'my-2 d-none';
+            nokDiv.className = 'my-2';
+            btnSubmit.classList.add('disabled'); // 버튼 비활성화
+        }
+    }
     
 });
